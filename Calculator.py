3d92c1db57212765b200861f4eb41340cb159fd9
MKBastler <49870446+MKBastler@users.noreply.github.com>
2021-08-17 20:27:16 +0200

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,9 @@
 print ("Calculator program. You got following options: 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division") 
-number=int(input("Make your choice"))
-while (number>4 or number<1): 
+try:
+ number=int(input("Make your choice"))
+ while (number>4 or number<1): 
   number=int(input("Please select 1 for addition, 2 for subtraction, 3 for multiplication or 4 for division"))
-  
+except: print("You use the wrong input format")  
 try:
    if (number==1):
     x=float(input("Select the first number:"))
