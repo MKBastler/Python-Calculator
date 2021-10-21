203d84cc6c59af8a2a5c9699f7b5b4bb7dff047f
MKBastler <49870446+MKBastler@users.noreply.github.com>
2021-10-21 19:54:12 +0200

    Rudimentary comments

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,11 @@
+#This is a simple calculator using the console
 print ("Calculator program. You got following options: 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division") 
 try:
  number=int(input("Make your choice"))
  while (number>4 or number<1): 
   number=int(input("Please select 1 for addition, 2 for subtraction, 3 for multiplication or 4 for division"))
-except: print("You use the wrong input format")  
+except: print("You use the wrong input format")
+#Catch when letter is inserted  
 try:
    if (number==1):
     x=float(input("Select the first number:"))
@@ -29,5 +31,8 @@ try:
     z=x/y
     print (z)
 except ZeroDivisionError: print("You cannot devide through zero")
+#Catch deviding through zero
 except ValueError: print("Please use numbers")
+#Catch any other input error
 except: print("Please use the proper input methods")
+#Catch code related errors
